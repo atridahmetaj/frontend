777f05edfa91f4533b1df60aa934c1f206eba5a7
Dhimiter Bundo <b.dhimiter@yahoo.com>
2020-03-08 12:40:42 +0100

    Add components for user crud and list

--- projects/admin/src/lib/admin.module.ts
+++ projects/admin/src/lib/admin.module.ts
@@ -1,11 +1,13 @@
 import { CommonsModule } from '@ms-system/commons';
 import { NgModule } from '@angular/core';
 import { AdminComponent } from './admin.component';
+import { UserListComponent } from './users/components/user-list/user-list.component';
+import { UserCrudComponent } from './users/components/user-crud/user-crud.component';
 
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, UserListComponent, UserCrudComponent],
   imports: [
     CommonsModule
   ],
